c11cc391d0b4c079e716684c1f4fbb3c964be4f3
itRunTester <itrun.tester@gmail.com>
2020-10-08 19:31:50 +0200

    yml

--- src/test/java/pl/itrun/selectfrom/SelectfromApplicationTests.java
+++ src/test/java/pl/itrun/selectfrom/SelectfromApplicationTests.java
@@ -3,11 +3,11 @@ package pl.itrun.selectfrom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class SelectfromApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+class SelectfromApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
