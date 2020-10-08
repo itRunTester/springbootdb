5af1ba81b8cb9c57532a24a9ab5fccfd6571fe1a
itRunTester <itrun.tester@gmail.com>
2020-10-08 18:06:47 +0200

    First Commit

---
+++ src/main/java/pl/itrun/selectfrom/SelectfromApplication.java
@@ -0,0 +1,13 @@
+package pl.itrun.selectfrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectfromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectfromApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/itrun/selectfrom/SelectfromApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.itrun.selectfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectfromApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
